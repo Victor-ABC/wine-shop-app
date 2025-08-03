97e8f568784950450bc53ade9dc89f7905b7eb16
Victor Corbet <vcorbet24@gmail.com>
2025-08-03 14:55:44 +0200

    added logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
